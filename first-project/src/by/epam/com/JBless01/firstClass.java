1ae1f08055e22990739269329d579ecf3a941848
Viktar_Ivanou <viktar_ivanou@epam.com>
2017-10-04 11:37:55 +0300

    My first lessons done

---
+++ first-project/src/by/epam/com/JBless01/firstClass.java
@@ -0,0 +1,10 @@
+package by.epam.com.JBless01;
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow World");// TODO Auto-generated method stub
+
+	}
+
+}
